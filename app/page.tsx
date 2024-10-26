dcec4cdcd26e18e5160dcd2aee852055e58683d6
papito111 <adrian.kurzac@wp.pl>
2024-10-26 14:36:09 +0200

    second

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see yours changsdf.</li>
+          <li>Save and changsdf.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
